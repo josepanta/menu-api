6d137ca9b55cebc37d6ee662d7cb5f9ccedd4592
josepanta <josepanta63@gmail.com>
2021-09-29 13:36:05 -0500

    Inicio

---
+++ src/test/java/dev/josepanta/menuapi/MenuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.josepanta.menuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
